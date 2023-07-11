2e132154e815bcea2a40983e0a99c1f38dd5b959
Laercio Luiz Barbosa <llbarbosa72@hotmail.com>
2023-07-11 17:02:22 -0300

    Acão de redirecionar para a Home.

--- src/components/Form/Login/Login.component.jsx
+++ src/components/Form/Login/Login.component.jsx
@@ -1,10 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
 export const FormLoginComponent = () => {
+    const navigate = useNavigate()
+    const redirectToLogin = () => {
+        navigate('/home')
+    }
     return (
-        <>
-        <p>FormLoginComponent is Render</p>
+       
+         <form onSubmit={redirectToLogin}>
+            <legend>Login</legend>
+            <div className="input-group">
+                <label htmlFor="email">E-mail</label>
+                <input type="email" id="email" placeholder="Digite seu email"/>
+            </div>
+            <div className="input-group">
+                <label htmlFor="password">Senha</label>
+                <input type="password" id="email" placeholder="Digite sua senha"/>
+            </div>
+            <button type="submit">Logar</button>
+        </form>
         
-        
-        </>
+       
 
 
 
--- src/pages/Login/Login.page.jsx
+++ src/pages/Login/Login.page.jsx
@@ -1,17 +1,10 @@
 import { FormLoginComponent } from "../../components/Form/Login/Login.component"
+
 export const LoginPage = () => {
     return (
-        <form>
-            <legend>Login</legend>
-            <div className="input-group">
-                <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" placeholder="Digite seu email"/>
-            </div>
-            <div className="input-group">
-                <label htmlFor="password">Senha</label>
-                <input type="password" id="email" placeholder="Digite sua senha"/>
-            </div>
-            <button type="submit">Logar</button>
-        </form>
+        <>
+        <FormLoginComponent />
+        
+        </>
     )
 }
